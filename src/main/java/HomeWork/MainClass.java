7eb7f3340a5a9cde2eb18640c95fa70d5fc5ed60
BorisLaskin <varislaskin@list.ru>
2024-02-20 21:49:27 +0300

    withExamples

--- src/main/java/HomeWork/MainClass.java
+++ src/main/java/HomeWork/MainClass.java
@@ -11,12 +11,14 @@ import java.io.IOException;
 public class MainClass {
     public static void main(String[] args) {
         //Иванов Иван Иванович 12.01.2016 male 89123458912
+        //Иванов Петр Иванович 01.01.2010 male 89123458912
+        //Петров Петр Петрович 01.11.2000 male 89123458912
         ReaderFromConsole myReader = new ReaderFromConsole();
         System.out.println("Введите данные о человеке в формате\n" +
                 "Фамилия Имя Отчество ЧЧ.ММ.ГГГГ(датаРождения) пол(male/female) телефон");
         Human human = myReader.readNewHuman();
         String filename = human.getFamily();
-        try(FileWriter writer = new FileWriter(filename,true)){
+        try(FileWriter writer = new FileWriter(filename+".txt",true)){
             writer.write(human.toString()+"\n");
         }
         catch (IOException e){
